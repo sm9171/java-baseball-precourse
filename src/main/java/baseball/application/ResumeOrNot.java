6bdc43a1c57766601fe2566eed50b0a41e6adedb
sm9171 <sm9171@nate.com>
2022-09-30 09:22:30 +0900

    feat(ResumeOrNot): 질의어 클래스 생성

---
+++ src/main/java/baseball/application/ResumeOrNot.java
@@ -0,0 +1,37 @@
+package baseball.application;
+
+import java.util.Objects;
+
+public class ResumeOrNot {
+
+	private final int opinion;
+
+	public ResumeOrNot(String userReply) {
+		isValidLength(userReply);
+
+		this.opinion = userReply.charAt(0) - '0';
+		isValidOpinionRange(opinion);
+	}
+
+	private void isNullValue(String userReply) {
+		if (Objects.isNull(userReply)) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void isValidLength(String userReply) {
+		if (userReply.length() != 1) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void isValidOpinionRange(int opinion) {
+		if ((opinion != 1) && (opinion != 2)) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public boolean isResume() {
+		return (opinion == 1);
+	}
+}
